ef61e0dc85cc793b1ceb0ba90f7cf99e4228a496
dhulipalla-vijayasri <vijayasridhulipalla95@gmail.com>
2021-07-16 04:15:11 -0700

    todaytask

---
+++ todaytask/ex1.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $('#btn1').click(function () {
+        $('#name').css('display', 'block')
+        $('#span1').css('display', 'flex')
+    })
+
+    $('#span1').click(function () {
+        $('#name').css('display', 'none')
+        $('#span1').css('display', 'none')
+    })
+
+})
+
+$(document).ready(function () {
+
+    $('#btn2').click(function () {
+        $('#email').css('display', 'block')
+        $('#span2').css('display', 'flex')
+    })
+
+    $('#span2').click(function () {
+        $('#email').css('display', 'none')
+        $('#span2').css('display', 'none')
+    })
+    
+    
+
+})
